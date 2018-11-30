bc6b73717f472ca80e5a0bdd3a024ea2d7135383
KW <werniokrzysztof@gmail.com>
2018-11-30 13:02:35 +0100

    powrot do hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world! piękny świat \n coś więcej"
+    render html: "hello, world! piękny świat"
   end
 
 end
