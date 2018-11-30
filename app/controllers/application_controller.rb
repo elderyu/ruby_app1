412f7d8372471a5ded3293e5bf0bee63ca376eb8
KW <werniokrzysztof@gmail.com>
2018-11-30 12:54:12 +0100

    dluzszy tekst

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world! piękny świat"
+    render html: "hello, world! piękny świat \n coś więcej"
   end
 
 end
