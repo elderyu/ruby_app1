52c8b015c3519c9aedb7a82cde86394d87fb5642
KW <werniokrzysztof@gmail.com>
2018-12-01 10:48:43 +0100

    corrected mistake

--- db/migrate/20181201092717_create_microposts.rb
+++ db/migrate/20181201092717_create_microposts.rb
@@ -1,7 +1,7 @@
 class CreateMicroposts < ActiveRecord::Migration[5.2]
   def change
     create_table :microposts do |t|
-      t.text_user :content
+      t.text :content
       t.integer :user_id
 
       t.timestamps
